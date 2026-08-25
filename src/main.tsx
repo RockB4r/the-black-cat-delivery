1607525122f2db0bf0afb744c9f6de06aa146e37
Enrique <theblackcat.rockcafe@outlook.com>
2026-08-25 03:38:58 -0500

    Add returns and cancellations policy

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ComplaintsBook } from './ComplaintsBook.tsx'
-import { PrivacyPolicy, TermsAndConditions } from './LegalPages.tsx'
+import { PrivacyPolicy, ReturnsPolicy, TermsAndConditions } from './LegalPages.tsx'
 import { StaffPortal } from './staff/StaffPortal.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {window.location.pathname === '/staff' ? <StaffPortal /> : window.location.pathname === '/libro-de-reclamaciones' ? <ComplaintsBook /> : window.location.pathname === '/terminos-y-condiciones' ? <TermsAndConditions /> : window.location.pathname === '/politica-de-privacidad' ? <PrivacyPolicy /> : <App />}
+    {window.location.pathname === '/staff' ? <StaffPortal /> : window.location.pathname === '/libro-de-reclamaciones' ? <ComplaintsBook /> : window.location.pathname === '/terminos-y-condiciones' ? <TermsAndConditions /> : window.location.pathname === '/politica-de-privacidad' ? <PrivacyPolicy /> : window.location.pathname === '/politica-cambios-devoluciones' ? <ReturnsPolicy /> : <App />}
   </StrictMode>,
 )
